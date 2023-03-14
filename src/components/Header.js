047c7396cfa8b0ccb8649f3468d9e36d914997fc
Tingwutobong Okonson <tidarkson@gmail.com>
2023-03-14 01:35:32 +0100

    commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <>
-            <header className='border border-1 border-slate-700 text-white text-2xl mb-10 px-5'>
+            <header className='sticky top-0 z-30 w-full border border-1 border-slate-700 text-slate-700 bg-slate-100 text-2xl mb-10 px-5'>
                 <h1>FXSHION!</h1>
             </header>
         </>
